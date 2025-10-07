5fe3e9700999c4a5cdcb7cee33846c965e40da07
AvgJoe-cpu <129824187+AvgJoe-cpu@users.noreply.github.com>
2025-10-07 13:27:23 +0200

    chore: remove hello bin and main; make r4g_app a stub lib

---
+++ target/debug/deps/hello-13456efc3ba66f3f.d
@@ -0,0 +1,5 @@
+/Users/jona/RustroverProjects/r4gs/target/debug/deps/hello-13456efc3ba66f3f.d: r4g_app/src/bin/hello.rs
+
+/Users/jona/RustroverProjects/r4gs/target/debug/deps/hello-13456efc3ba66f3f: r4g_app/src/bin/hello.rs
+
+r4g_app/src/bin/hello.rs:
---
+++ target/debug/hello.d
@@ -0,0 +1,1 @@
+/Users/jona/RustroverProjects/r4gs/target/debug/hello: /Users/jona/RustroverProjects/r4gs/r4g_app/src/bin/hello.rs
